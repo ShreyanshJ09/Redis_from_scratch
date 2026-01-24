857c5dfa1b05d3f663e0f7c95eb76d8a9270541f
ShreyanshJO9 <shreyanshjangitwar@gmail.com>
2026-01-24 23:02:41 +0530

    multiple request

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,14 @@ public class Main {
          serverSocket.setReuseAddress(true);
          // Wait for connection from client.
          clientSocket = serverSocket.accept();
+         while (true) {
+            // Read input from client.
+            byte[] input = new byte[1024];
+            clientSocket.getInputStream().read(input);
+            String inputString = new String(input).trim();
+            System.out.println("Received: " + inputString);
+            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+          }
        } catch (IOException e) {
          System.out.println("IOException: " + e.getMessage());
        } finally {
